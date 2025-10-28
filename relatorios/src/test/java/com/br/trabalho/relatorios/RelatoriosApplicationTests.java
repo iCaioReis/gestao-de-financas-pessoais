d508ec202d21508fb52e1d4c00b727b3419d7db0
iCaioReis <caioreis3186@gmail.com>
2025-10-27 21:12:13 -0300

    Configuração serviço usuarios

---
+++ relatorios/src/test/java/com/br/trabalho/relatorios/RelatoriosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.trabalho.relatorios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelatoriosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
